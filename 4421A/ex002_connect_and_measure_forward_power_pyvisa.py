bae5722565d5bf502a744845af3205e796b1f8f8
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2024-03-07 12:16:01 -0500

    Update ex002_connect_and_measure_forward_power_pyvisa.py
    Updated the header info.

--- 4421A/ex002_connect_and_measure_forward_power_pyvisa.py
+++ 4421A/ex002_connect_and_measure_forward_power_pyvisa.py
@@ -1,19 +1,7 @@
 """
 Example Description:
-        This example application demonstrates how to use the DAQ6510 to perform
-        complex multi-channel, mixed function scanning in a production-test
-        environment. The DAQ6510 can perform more than one function in a multichannel
-        scan, providing a range of dataacquisition options in a single test.
-
-        In this production environment the DAQ6510 is:
-        - Integrated into a test stand.
-        - Wired to a fixture that is connected to an active device under test (DUT).
-        - Quickly capturing DC volts and current, temperature, and AC volts and current.
-
-        Prior to the start of the scan, you can step through each of the configured
-        channels on the DAQ6510, which allows you to troubleshoot the test
-        configuration. This allows you to view the readings of individually closed
-        channels to ensure that connections to the DUT are secure.
+        This example shows how to connect to the 4421A then query its
+        measured forward and reflected power values.
 
 @verbatim
 
